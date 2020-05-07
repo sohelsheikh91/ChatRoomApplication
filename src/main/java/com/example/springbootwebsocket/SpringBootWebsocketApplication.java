4b928b679aa6996b2bf83bfa97428409ae948d2b
Sohel Sheikh <sohelsheikh91@gmail.com>
2020-05-07 12:59:46 +0530

    SpringBootServletInitializer added in main class for Tomcat

--- src/main/java/com/example/springbootwebsocket/SpringBootWebsocketApplication.java
+++ src/main/java/com/example/springbootwebsocket/SpringBootWebsocketApplication.java
@@ -2,9 +2,15 @@ package com.example.springbootwebsocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootWebsocketApplication {
+public class SpringBootWebsocketApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootWebsocketApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebsocketApplication.class, args);
