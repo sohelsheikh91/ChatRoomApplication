9a01b16e30a6189e05bba095ed814a47007f8104
Sheikh, Sohel <sohelsheikh91@gmail.com>
2022-08-04 00:55:20 +0530

    Removed Unnecessary Imports

--- src/main/java/com/example/springbootwebsocket/config/SchedulerConfig.java
+++ src/main/java/com/example/springbootwebsocket/config/SchedulerConfig.java
@@ -1,11 +1,9 @@
 package com.example.springbootwebsocket.config;
 
-import com.example.springbootwebsocket.model.UserResponce;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @EnableScheduling
 @Configuration
--- src/main/java/com/example/springbootwebsocket/config/WebSocketConfig.java
+++ src/main/java/com/example/springbootwebsocket/config/WebSocketConfig.java
@@ -1,16 +1,11 @@
 package com.example.springbootwebsocket.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.simp.user.SimpSession;
-import org.springframework.messaging.simp.user.SimpUser;
-import org.springframework.messaging.simp.user.SimpUserRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-import java.util.Set;
 
 @Configuration
 @EnableWebSocketMessageBroker
